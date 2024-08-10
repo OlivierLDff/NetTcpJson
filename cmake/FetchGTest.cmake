c253ed7bcae0f800068e1d86a7addd2e20802912
Olivier Le Doeuff <olivier.ldff@gmail.com>
2024-08-10 16:29:54 +0200

    ⬆️ googletest v1.15.2

--- cmake/FetchGTest.cmake
+++ cmake/FetchGTest.cmake
@@ -1,7 +1,7 @@
 include(FetchContent)
 
 set(GTEST_REPOSITORY "https://github.com/google/googletest" CACHE STRING "googletest repository url")
-set(GTEST_TAG "master" CACHE STRING "googletest git tag")
+set(GTEST_TAG "v1.15.2" CACHE STRING "googletest git tag")
 
 FetchContent_Declare(
   googletest
