818d9e64ff74605fc469b73b54bcc6a99c18e958
Olivier Le Doeuff <olivier.ldff@gmail.com>
2024-08-24 17:22:08 +0200

    ⬆️ Qt6 support

--- cmake/FetchNetTcp.cmake
+++ cmake/FetchNetTcp.cmake
@@ -1,14 +1,22 @@
-include(FetchContent)
+include(${CMAKE_CURRENT_LIST_DIR}/CPM.cmake)
 
-set(NETTCP_REPOSITORY "https://github.com/OlivierLDff/NetTcp.git" CACHE STRING "NetTcp repository url")
-set(NETTCP_TAG master CACHE STRING "NetTcp git tag")
-
-FetchContent_Declare(
-  NetTcp
-  GIT_REPOSITORY ${NETTCP_REPOSITORY}
-  GIT_TAG        ${NETTCP_TAG}
+set(NETTCP_REPOSITORY
+    "https://github.com/OlivierLDff/NetTcp.git"
+    CACHE STRING "NetTcp repository url"
+)
+set(NETTCP_TAG
+    master
+    CACHE STRING "NetTcp git tag"
 )
 
-set(NETTCP_ENABLE_QML OFF CACHE BOOL "Embedded Debug Qml module for NetTcp class")
-set(NETTCP_FOLDER_PREFIX "Dependencies" CACHE STRING "Prefix folder for all NetTcp generated targets in generated project (only decorative)")
-FetchContent_MakeAvailable(NetTcp)
+set(NETTCP_FOLDER_PREFIX
+    "Dependencies"
+    CACHE STRING
+          "Prefix folder for all NetTcp generated targets in generated project (only decorative)"
+)
+
+CPMAddPackage(
+  NAME NetTcp
+  GIT_REPOSITORY ${NETTCP_REPOSITORY}
+  GIT_TAG ${NETTCP_TAG}
+)
